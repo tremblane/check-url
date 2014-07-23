b97743fb87cd7c997b7208790ce9ed61c6d63f1f
Jason <jason.dew@unc.edu>
2014-07-23 17:01:10 -0400

    Added spaces for formatting

--- check-multi-url.sh
+++ check-multi-url.sh
@@ -3,5 +3,5 @@
 for url in $*
 do
     echo $url
-    check-url.sh $url | grep -e ^HTTP -e^URL-Effective: -e^Error: | sed 's/^URL-Effective: //' | sed 's/^/ /'
+    check-url.sh $url | grep -e ^HTTP -e^URL-Effective: -e^Error: | sed 's/^URL-Effective: //' | sed 's/^/   /'
 done
